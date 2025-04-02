5876ee43fbdf07ec1780d754b047009239cc3d3f
borisbvf <borisbvf@outlook.com>
2025-04-02 18:31:45 +0900

    Add Localization Manager service. Add Resource files for strings in English and Russian

---
+++ Services/LocalizationManager.cs
@@ -0,0 +1,24 @@
+﻿using BaseMobile.Resources.Localization;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace BaseMobile.Services;
+public class LocalizationManager : INotifyPropertyChanged
+{
+	private LocalizationManager()
+	{
+		AppResources.Culture = CultureInfo.CurrentCulture;
+	}
+
+	public static LocalizationManager Instance = new LocalizationManager();
+
+	public object this[string resourceKey] => AppResources.ResourceManager.GetObject(resourceKey, AppResources.Culture) ?? Array.Empty<byte>();
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public void SetCulture(CultureInfo culture)
+	{
+		AppResources.Culture = culture;
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+	}
+}
