e34810ef45fba1fe210ee220030cef026682aeac
borisbvf <borisbvf@outlook.com>
2025-04-02 17:57:54 +0900

    Add constants for settings

---
+++ Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseMobile;
+public class Constants
+{
+	public const string AppThemeKey = "AppTheme";
+	public const string LanguageKey = "Language";
+
+	public const string RussianLang = "Русский";
+	public const string EnglishLang = "English";
+}
