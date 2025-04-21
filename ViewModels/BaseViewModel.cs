b8c97043775b87c4d2ed241a982ced5c8676e844
borisbvf <borisbvf@outlook.com>
2025-04-21 18:51:24 +0900

    Add INotifyPropertyChanged to BaseViewModel.

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 namespace BaseMobile.ViewModels;
-public class BaseViewModel
+public class BaseViewModel : INotifyPropertyChanged
 {
 	private bool isBusy;
 	private string? title;
